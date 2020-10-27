d4fe5ee884f17f4f9e354e64743aac5d9a613b2f
pvs1980@gmail.com <pvs1980@gmail.com>
2020-10-27 21:56:32 +0800

    modified on 20201027

--- main.js
+++ main.js
@@ -2,40 +2,60 @@ const express = require('express');
 const handlebars = require('express-handlebars')
 
 // configure the environment
-const PORT = parseInt(process.argv[2]) ||  3000 
+const PORT = parseInt(process.argv[2]) || 3000
 
 // create an instance of express
 const app = express()
 
 // configure handlebars
-app.engine('hbs', 
-    handlebars({ defaultLayout: 'default.hbs'})
+app.engine('hbs',
+    handlebars({ defaultLayout: 'default.hbs' })
 )
 app.set('view engine', 'hbs')
 
-app.get('/', 
+
+app.get('/',
     (req, resp) => {
         resp.status(200)
         resp.type('text/html')
-        resp.render('./index.html',
-            
-        )
+        resp.render('index')
     }
 )
 
-app.get('/roll', 
+
+var dice1;
+var dice2;
+app.get('/roll',
     (req, resp) => {
         resp.status(200)
         resp.type('text/html')
         resp.render('roll',
-            
+            {
+                dice1: (Math.floor(Math.random() * 6) + 1),
+                dice2: (Math.floor(Math.random() * 6) + 1)
+
+            }
+
         )
+    }
+)
+
+// configure the application
+app.use(
+    express.static(__dirname + '/static')
+)
+
+app.use(
+    (req, resp) => {
+        resp.status(404)
+        resp.type('text/html')
+        resp.sendFile(__dirname + '/static/404.html')
     }
 )
 
 //start express
 
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     //callback , execute after express has started
-    console.info(`Application started on port ${PORT} at ${new Date()}`);
+    console.info(`Application started on port ${PORT}`);
 })
