6c7985fe014bd3e887bd1ebfb21b5d0e514a787e
pvs1980@gmail.com <pvs1980@gmail.com>
2020-10-27 16:18:53 +0800

    My First FullStack App

---
+++ main.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const handlebars = require('express-handlebars')
+
+// configure the environment
+const PORT = parseInt(process.argv[2]) ||  3000 
+
+// create an instance of express
+const app = express()
+
+// configure handlebars
+app.engine('hbs', 
+    handlebars({ defaultLayout: 'default.hbs'})
+)
+app.set('view engine', 'hbs')
+
+app.get('/dice', 
+    (req, resp) => {
+        resp.status(200)
+        resp.type('text/html')
+        resp.render('dice',
+            
+        )
+    }
+)
+
+//start express
+
+app.listen(PORT, () =>{
+    //callback , execute after express has started
+    console.info(`Application started on port ${PORT} at ${new Date()}`);
+})
